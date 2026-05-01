b2b348504def1a17e21ec24c6d1829e666b0352e
Dada Vincent <dadaademolav@gmail.com>
2026-05-01 20:21:56 +0100

    fixed about

--- src/pages/admin/AdminProducts.tsx
+++ src/pages/admin/AdminProducts.tsx
@@ -222,7 +222,7 @@ export default function AdminProducts({ category }: AdminProductsProps) {
           ))}
         </div>
       ) : (
-        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-5">
           {filtered.map(p => (
             <div
               key={p.id}
